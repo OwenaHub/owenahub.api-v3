3fe24c91db4e3d4ccff34eecbfb005b9b06bf1bc
Ernest Haruna <92730168+ernestharuna@users.noreply.github.com>
2025-03-20 22:04:20 +0100

    Remove Project model and related files; update Sanctum configuration for stateful domains

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,11 +15,14 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
-
+        
+        $middleware->statefulApi();
+        
         $middleware->alias([
             'verified' => \App\Http\Middleware\EnsureEmailIsVerified::class,
         ]);
 
+        
         //
     })
     ->withExceptions(function (Exceptions $exceptions) {
